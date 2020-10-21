80c2c0bea6202cd8e5c71eb8f452b320438d3602
lumanya <shaibulumanya@gmail.com>
2020-10-21 23:07:03 +0300

    first commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+ 
+
+  void  setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Dar es salaam', flag: 'Tanzania.png', url: 'Africa/Nairobi');
+
+    await instance.getTime();
+   
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime,
+    });
+
+  }
+
+  @override
+  void initState() {    
+    super.initState();
+    setupWorldTime();
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      body: Center(        
+          child: SpinKitFadingCircle(
+           color: Colors.white,
+           size: 70.0,
+          ),
+      ),
+    );
+  }
+}
